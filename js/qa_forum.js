3409fe117835fbb8e5fbf4379128d0f53bec26aa
Geoff St. Pierre <serundeputy@gmail.com>
2017-01-08 09:32:35 -0500

    This gets the up-vote and down-vote ajax links in place and detects which was clicked.

--- js/qa_forum.js
+++ js/qa_forum.js
@@ -3,15 +3,13 @@
    * Add new command for reading a message.
    */
   Drupal.ajax.prototype.commands.getVotes = function(ajax, response, status){
-      // Place content in current-msg div.
-      $('#ajax-target').html(response.title);
-      $('#ajax-target').html(response.geoff);
-      $('#ajax-target').html('blargh ....');
-      console.log('did i make it here?');
-      // $('#current-msg p').html(response.content);
-      // // Remove from unread list.
-      // $('#msg-' + response.mid).remove();
-      // // Add message to read list.
-      // $('#read-msgs').append('<li>' + response.subject + '</li>');
+    // Place content in ajax-target div.
+    var cid = response.cid;
+    var uid = response.uid;
+    var type = ajax.element.classList[1];
+
+    $('#ajax-target').html(
+      cid + '::' + uid + type
+    );
   }
 })(jQuery, Drupal);
